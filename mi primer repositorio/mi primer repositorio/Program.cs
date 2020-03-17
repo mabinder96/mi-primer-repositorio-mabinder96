86a1dc9b270fcc121b575e95799b395e245a7a35
mabinder96 <mbinder@miuandes.cl>
2020-03-16 23:00:51 -0300

    add new writeline

--- mi primer repositorio/mi primer repositorio/Program.cs
+++ mi primer repositorio/mi primer repositorio/Program.cs
@@ -7,6 +7,7 @@ namespace mi_primer_repositorio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Como estan?");
         }
     }
 }
